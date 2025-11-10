537269dd1b341614d78674b05647e59b27ad76e9
Mikasa5266 <lyh244140591@whut.edu.cn>
2025-11-10 17:00:39 +0800

    项目全新版本

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Index from './views/index.vue';
+
+</script>
+
+<template>
+ <RouterView>
+ </RouterView>
+</template>
+
+<style scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Button } from 'ant-design-vue';
+
+</script>
+<template>
+    <Button>hello world</Button>
+</template>
